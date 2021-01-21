e1fd4cd3b234c9a9d3a7d3395427af9f4bc95730
Philipp Kollmar <ph-kollmar@web.de>
2021-01-21 18:05:19 +0100

    updated buiseness logic and added more color tests

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Pin from './Pin/Pin'
 import {RED, BLUE, YELLOW, GREEN} from "mastermind/src/colors"
+import { useState } from 'react';
+import { initialModel, createModel } from './model/model';
 
 function App() {
-
+  const [model,setModel] = useState(initialModel())
+  const {getAssumedColor} = createModel(model)
   return (
     <div>
-      <Pin color={RED} change = {() => {alert("Hallo Ansbach")}}></Pin>
-      <Pin color={BLUE}></Pin>
-      <Pin color={YELLOW}></Pin>
-      <Pin color={GREEN}></Pin>
+      <Pin color={getAssumedColor(0)} change = {() => {setModel({assumedColors: [RED, GREEN, BLUE, YELLOW]})}}></Pin>
+      <Pin color={getAssumedColor(1)} change = {() => {alert(model)}}></Pin>
+      <Pin color={getAssumedColor(2)} ></Pin>
+      <Pin color={getAssumedColor(3)}></Pin>
     </div>
 
   );
